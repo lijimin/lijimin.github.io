31741dfb0222e1fd438f1f99e0ed69a1b446f777
lijimin <15868138095@163.com>
2017-11-06 14:07:40 +0800

    v1

--- bb2.js
+++ bb2.js
@@ -1,8 +1,8 @@
 var bubbles = [
-    {label:"主页", img:"img/imgs/zyl.jpg", href:"/data0/work/git-repo/github/lijimin/lijimin.github.io/index.html"},
-    {label:"简历", img:"img/imgs/jll.jpg", href:"/data0/work/git-repo/github/lijimin/lijimin.github.io/jianli.html"},
-    {label:"作品", img:"img/imgs/zpl.jpg", href:"/data0/work/git-repo/github/lijimin/lijimin.github.io/inin.html"},
-    {label:"其他", img:"img/imgs/qtl.jpg", href:"/data0/work/git-repo/github/lijimin/lijimin.github.io/notes.html"}
+    {label:"主页", img:"img/imgs/zyl.jpg", href:"https://lijimin.github.io/index.html"},
+    {label:"简历", img:"img/imgs/jll.jpg", href:"https://lijimin.github.io/jianli.html"},
+    {label:"作品", img:"img/imgs/zpl.jpg", href:"https://lijimin.github.io/inin.html"},
+    {label:"其他", img:"img/imgs/qtl.jpg", href:"https://lijimin.github.io/notes.html"}
 ];
 
 var svgns = "http://www.w3.org/2000/svg";
